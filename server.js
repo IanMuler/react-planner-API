450fc20e79e2a86c97875d5bbb9f0923597482c1
IanMuler <yoelmuler@gmail.com>
2022-07-25 22:09:13 -0300

    new port

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ router(app); // register routes
 db(process.env.DB_CONNECT); // connect to db
 
 app.listen(port, () => {
-  console.log("Example app listening on port 3000!");
+  console.log("Example app listening on port!");
 });
