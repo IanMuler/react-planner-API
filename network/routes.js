6a9ef3eedd21cbec7c330442d9e94feaae380360
IanMuler <yoelmuler@gmail.com>
2022-07-25 21:52:54 -0300

    home

--- network/routes.js
+++ network/routes.js
@@ -3,6 +3,9 @@ const state = require("../components/state/network");
 
 const routes = function (server) {
   server.use("/api/state", state);
+  server.get("/", (req, res) => {
+    res.send("React-Planner-API");
+  });
 };
 
 module.exports = routes;
