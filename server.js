4b83d857f0444d354dcf738e252c273cb125d022
IanMuler <yoelmuler@gmail.com>
2022-07-27 11:56:07 -0300

    cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = require("./network/routes");
 const port = process.env.PORT || 3000;
 const db = require("./db");
+var cors = require("cors");
 require("dotenv").config({ path: ".env" });
 
 const app = express(); // create express app
 app.use(express.json()); // recieves json data
 app.use(express.urlencoded({ extended: true })); // accepts form data
+app.use(cors()); // allows cross-origin requests
 
 router(app); // register routes
 
